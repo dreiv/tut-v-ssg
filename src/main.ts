189c1dba8aa0e74610e78355fb37ff0fa248c410
Voicu Andrei <dreivoicu@gmail.com>
2026-07-09 09:26:04 +0300

    ssg

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { ViteSSG } from "vite-ssg/single-page";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+export const createApp = ViteSSG(App);
