d24e9150bd81e057bdb38bc41da6d4a840db6fa5
Voicu Andrei <dreivoicu@gmail.com>
2026-07-09 09:39:55 +0300

    trial of critters

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import type { ViteSSGOptions } from "vite-ssg";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +14,12 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  ssgOptions: {
+    formatting: "minify",
+    crittersOptions: {
+      reduceInlineStyles: false,
+      preload: "swap",
+      publicPath: "/tut-v-ssg/",
+    },
+  } as ViteSSGOptions,
 });
